b5303d02ba8fad6d89f3599d396acaeb56f14a20
yujindong <yujd@133.cn>
2019-04-12 11:51:52 +0800

    feat: rm device package

--- packages/device/__tests__/device.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const device = require('..');
-
-describe('device', () => {
-    it('needs tests');
-});
--- packages/device/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-if (process.env.NODE_ENV === "production") {
-    module.exports = require("./dist/cjs/device.min.js");
-} else {
-    module.exports = require("./dist/cjs/device.js");
-}
