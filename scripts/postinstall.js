6322b4eb4390fe280815caf2fd00e6b66097ed8e
yujindong <yujd@133.cn>
2019-05-28 10:45:19 +0800

    feat: 111

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,9 +3,6 @@ const execSync = require("child_process").execSync;
 function exec(cmd) {
     execSync(cmd, { stdio: "inherit", env: process.env });
 }
-exec("pwd")
-exec("cat ~/.npmrc");
-exec("cat $HOME");
 if (process.env.CI) {
     if (process.env.TRAVIS_BRANCH !== "website") {
         exec("lerna bootstrap --ci --ignore website --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
