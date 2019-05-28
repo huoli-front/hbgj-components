ef4a31675714298b458f1011da918b059e09fa70
yujindong <yujd@133.cn>
2019-05-28 10:29:20 +0800

    fix

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,14 +3,13 @@ const execSync = require("child_process").execSync;
 function exec(cmd) {
     execSync(cmd, { stdio: "inherit", env: process.env });
 }
-
-exec("npm config ls");
+exec("pwd")
 exec("cat ~/.npmrc");
 if (process.env.CI) {
     if (process.env.TRAVIS_BRANCH !== "website") {
-        exec("lerna bootstrap --ci --ignore website --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
+        exec("lerna bootstrap --ci --ignore website --registry https://registry.npmjs.org/");
     } else {
-        exec("lerna bootstrap --ci --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
+        exec("lerna bootstrap --ci --registry https://registry.npmjs.org/");
     }
 } else {
     exec("lerna bootstrap --no-ci --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
