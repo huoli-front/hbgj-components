022c1cfda80fa061f78cc7f61e07e7ef9abee061
yujindong <yujd@133.cn>
2019-05-28 10:56:31 +0800

    feat: 111

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,13 @@ const execSync = require("child_process").execSync;
 function exec(cmd) {
     execSync(cmd, { stdio: "inherit", env: process.env });
 }
+exec('echo "before installaaaaaaaaaaaa"')
+exec('echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > "$HOME/.npmrc"')
+exec('echo "//registry.npm.rsscc.cn/repository/hbgj-npm/:_authToken=$HL_TOKEN" >> "$HOME/.npmrc"')
+exec('echo "//registry.npm.rsscc.cn/repository/hbgj-fe/:_authToken=$HL_TOKEN" >> "$HOME/.npmrc"')
+exec('echo "registry=http://registry.npm.rsscc.cn/repository/hbgj-fe/" >> "$HOME/.npmrc"')
+exec('cat $HOME/.npmrc')
+
 if (process.env.CI) {
     if (process.env.TRAVIS_BRANCH !== "website") {
         exec("lerna bootstrap --ci --ignore website --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
