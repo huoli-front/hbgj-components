bc67300b38094ae12225cf2f5302dc2c44a286a3
yujindong <yujd@133.cn>
2019-05-28 10:33:23 +0800

    fix

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,9 +7,9 @@ exec("pwd")
 exec("cat ~/.npmrc");
 if (process.env.CI) {
     if (process.env.TRAVIS_BRANCH !== "website") {
-        exec("lerna bootstrap --ci --ignore website --registry https://registry.npmjs.org/");
+        exec("lerna bootstrap --ci --ignore website --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
     } else {
-        exec("lerna bootstrap --ci --registry https://registry.npmjs.org/");
+        exec("lerna bootstrap --ci --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
     }
 } else {
     exec("lerna bootstrap --no-ci --registry http://registry.npm.rsscc.cn/repository/hbgj-fe/");
