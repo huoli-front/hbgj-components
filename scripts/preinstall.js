2106b37b5c1accde0f0c510dc19cbc2bb03313dd
yujindong <yujd@133.cn>
2020-06-28 11:08:46 +0800

    feat: update publish

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,10 +1,10 @@
-const execSync = require("child_process").execSync;
+// const execSync = require("child_process").execSync;
 
-function exec(cmd) {
-  execSync(cmd, { stdio: "inherit", env: process.env });
-}
-exec('echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > "$HOME/.npmrc"')
-exec('echo "//registry.npm.rsscc.cn/repository/hbgj-npm/:_authToken=$HL_TOKEN" >> "$HOME/.npmrc"')
-exec('echo "//registry.npm.rsscc.cn/repository/hbgj-fe/:_authToken=$HL_TOKEN" >> "$HOME/.npmrc"')
-exec('echo "registry=http://registry.npm.rsscc.cn/repository/hbgj-fe/" >> "$HOME/.npmrc"')
-exec('cat $HOME/.npmrc')
+// function exec(cmd) {
+//   execSync(cmd, { stdio: "inherit", env: process.env });
+// }
+// exec('echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > "$HOME/.npmrc"')
+// exec('echo "//registry.npm.rsscc.cn/repository/hbgj-npm/:_authToken=$HL_TOKEN" >> "$HOME/.npmrc"')
+// exec('echo "//registry.npm.rsscc.cn/repository/hbgj-fe/:_authToken=$HL_TOKEN" >> "$HOME/.npmrc"')
+// exec('echo "registry=http://registry.npm.rsscc.cn/repository/hbgj-fe/" >> "$HOME/.npmrc"')
+// exec('cat $HOME/.npmrc')
