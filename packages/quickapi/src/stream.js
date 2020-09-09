84b6dffeb25741e7c8953ec76e54e39bec0d2670
yujindong <yujd@133.cn>
2020-09-09 11:06:11 +0800

    feat: fix

--- packages/quickapi/src/stream.js
+++ packages/quickapi/src/stream.js
@@ -2,6 +2,7 @@
 import config from "./config";
 
 export function send(message) {
+  window.console.log("quickapi-send:" + message);
   if (config.isQuickApp) {
     system.postMessage(message);
   }
